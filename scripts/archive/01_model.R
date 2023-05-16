a7ecd08aada3b9c15ae4c091100bf6229a861405
cabuelow <christinabuelow@gmail.com>
2023-05-16 12:25:14 +1000

    fix model

--- scripts/archive/01_model.R
+++ scripts/archive/01_model.R
@@ -2,16 +2,15 @@
 
 # set up some initial params
 
-dens <- 0.415 # juveniles per hectare post restoration
+dens <- 0.415 # juveniles per hectare enhanced by restoration
 m <- 0.78 # annual mortality rate
 t_max <- 7 # maximum age
 t_0 <- -1.1 # theoretical age when length is 0
-t_harv <- 1 # age of recruitment to fishery
+t_harv <- 2 # age of recruitment to fishery
 l_asym <- 22 # asymptotic size (length in cm)
 Ks <- 0.33 # Brody growth coef 
 a <- 0.0316 # intercept of length-weight relationship
 b <- 3.03 # slope of length-weight relationship
-area_restor <- 1 # area restored in hectares
 years <- 25 # number of years since restoration
 
 # set up as a dataframe
@@ -19,27 +18,25 @@ years <- 25 # number of years since restoration
 dat <- data.frame(species = 'Snapper', dens = dens,
                   m = m, t_max = t_max, t_0 = t_0, t_harv = t_harv,
                   l_asym = l_asym, Ks = Ks, a = a, b = b)
-#write.csv(dat, 'data/template-df.csv', row.names = F)
 
 # function to return a dataframe of densities, lengths and weights in each year
 
 mod_enhance <- function(dens, spp, mf, m, t_max, t_0, t_harv, l_asym, Ks, a, b, area_restor, years){
-  df <- data.frame(species = NA, male_female = NA, year = NA, denhance = NA, length = NA, weight = NA, weight_i = NA, biomass = NA, biomass_i = NA, benhance = NA)
+  df <- data.frame(species = NA, male_female = NA, year = NA, d_ind_ha = NA, length_cm = NA, weight_g = NA, weight_g_i = NA, bio_enhance_g_ha = NA, cumul_bio_enhance_g_ha = NA)
   for(i in 1:years){
     df[i,1] <- spp # spp
     df[i,2] <- mf # male or female
-    df[i,3] <- i # year
-    df[i,4] <- dens*exp(-m*(i-0.5))
-    df[i,5] <- l_asym*(1-exp(-Ks*(i-t_0))) # estimate length using von bert eqn
-    df[i,6] <- if(i<=t_max){a*df[i,5]^b}else(df[t_max,6]) # convert length to weight, only if fish is alive
+    df[i,3] <- i # year post restoration
+    df[i,4] <- if(i<=t_max)(dens*exp(-m*(i-0.5)))else(0) # density of recruited individuals in each year, all die after max age
+    df[i,5] <- if(i<=t_max)(l_asym*(1-exp(-Ks*(i-t_0))))else(0) # estimate length using von bert eqn, only while fish are alive
+    df[i,6] <- if(i<=t_max){a*df[i,5]^b}else(0) # convert length to weight, only if fish is alive
   }
   for(i in 1:years){
-    df[i,7] <- df[i+1,6] - df[i,6] # get the incremental increase in weight for each time interval (i.e. year)
-    df[i,8] <- df[i,4]*df[i,6] # convert weight to biomass
-    df[i,9] <- df[i,4]*df[i,7] # convert weight to biomass
+    df[i,7] <- if(i<t_max)(df[i+1,6] - df[i,6])else(0) # get the incremental increase in weight for each time interval (i.e. year)
   }
   for(i in t_harv:years){
-    df[i,10] <- if(i>t_max){(df[t_harv, 'biomass'] + (sum(df[t_harv:t_max, 'biomass_i'], na.rm = T)))*area_restor}else((df[t_harv, 'biomass'] + (sum(df[t_harv:i, 'biomass_i'], na.rm = T)))*area_restor)
+    df[i,8] <- (df[t_harv, 'weight_g'] + (sum(df[t_harv:i, 'weight_g_i'], na.rm = T)))*df[i,4]
+    df[i,9] <- if(i==t_harv){df[i,8]}else{df[i-1,9]+df[i,8]}
   }
   return(df)
 }
@@ -53,5 +50,5 @@ enhancement <- mod_enhance(dat$dens, dat$species, NA, dat$m, dat$t_max, dat$t_0,
 # have a look 
 
 head(enhancement)
-plot(x = enhancement$year, y = enhancement$benhance)
+plot(x = enhancement$year, y = enhancement$cumul_bio_enhance_g_ha)
 
