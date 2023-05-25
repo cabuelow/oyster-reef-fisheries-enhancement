ecdddb7f446c3b9c8e21d82519f7ab94f51d59e1
cabuelow <christinabuelow@gmail.com>
2023-05-25 16:14:46 +1000

    add wrangling script

---
+++ scripts/01_wrangle-data.R
@@ -0,0 +1,32 @@
+# wrangle and join life history parameters and juvenile density enhancements
+
+library(tidyverse)
+
+# life history parameters
+
+dat <- read.csv('data/fish-dat.csv') %>% 
+  mutate(m = ifelse(is.na(use_m), m_Then2014, m)) %>% # here use literature 'm' values when confident, otherwise use Then eqn
+  mutate(t_harv = ifelse(harvested == 'y', t_harv, t_maturity)) %>% # for species that aren't harvested, setting their t_harv value to age at maturity instead
+  mutate(t_harv = ifelse(t_harv < 1, 1, t_harv)) %>% # if time of harvest or maturity is less than 1 year, set it to first year (otherwise can model at finer resolution)
+  pivot_longer(cols = c(m, m_Then2014), names_to = 'mortality', values_to = 'mortality_val') %>% 
+  data.frame()
+
+# juvenile densities
+
+dat_density <- read.csv('data/juvenile_densities_per_site.csv') %>% 
+  pivot_wider(id_cols = c(Site, common_name), names_from = 'Habitat_agg', 
+              values_from = c('densitymean', 'densityvar', 'densitystd', 'n_y')) %>% 
+  mutate(d = densitymean_Reef - densitymean_Unstructured) %>% # calculate density enhancement
+  filter(d > 0) %>% # filter for only positive density enhancements
+  #mutate(d_se = (sqrt((densityvar_Reef*n_y_Reef)+(densityvar_Unstructured*n_y_Unstructured))/(n_y_Reef+n_y_Unstructured))/sqrt((n_y_Reef+n_y_Unstructured)/2)) %>% # calculate standard error via weighted average of variances
+  mutate(d_se = densitystd_Reef/sqrt(n_y_Reef)) %>% # all of the densities on unstructured are 0, so will just use standard error of reef
+  data.frame()
+
+# join to other data frame
+
+dat_final <- dat_density %>% 
+  select(Site, common_name, d, d_se) %>% 
+  rename(d_site = 'Site', species = 'common_name') %>% 
+  left_join(dat, by = 'species')
+
+write.csv(dat_final, 'data/wrangled-dat.csv', row.names = F)
--- scripts/archive/01_model.R
+++ scripts/archive/01_model.R
@@ -2,11 +2,11 @@
 
 # set up some initial params
 
-dens <- 0.415 # juveniles per hectare enhanced by restoration
+dens <- 0.415 # juveniles per 100 m2 enhanced by restoration
 m <- 0.78 # annual mortality rate
-t_max <- 7 # maximum age
+t_max <- 7.5 # maximum age
 t_0 <- -1.1 # theoretical age when length is 0
-t_harv <- 2 # age of recruitment to fishery
+t_harv <- 2.5 # age of recruitment to fishery
 l_asym <- 22 # asymptotic size (length in cm)
 Ks <- 0.33 # Brody growth coef 
 a <- 0.0316 # intercept of length-weight relationship
@@ -15,39 +15,38 @@ years <- 25 # number of years since restoration
 
 # set up as a dataframe
 
-dat <- data.frame(species = 'Snapper', dens = dens,
+dat <- data.frame(species = 'Snapper', dens = dens, mf = NA,
                   m = m, t_max = t_max, t_0 = t_0, t_harv = t_harv,
                   l_asym = l_asym, Ks = Ks, a = a, b = b)
 
 # function to return a dataframe of densities, lengths and weights in each year
 
-mod_enhance <- function(dens, spp, mf, m, t_max, t_0, t_harv, l_asym, Ks, a, b, area_restor, years){
-  df <- data.frame(species = NA, male_female = NA, year = NA, d_ind_ha = NA, length_cm = NA, weight_g = NA, biomass_g_ha = NA, gross_biomass_g_ha = NA, weight_g_i = NA, biomass_g_ha_i = NA, net_biomass_g_ha = NA)
+mod_enhance <- function(dens, spp, mf, m, t_max, t_0, t_harv, l_asym, Ks, a, b, years){
+  df <- data.frame(species = spp, male_female = mf, year = 0, d_ind_m2 = 0, length_cm = 0, weight_g = 0, biomass_g_100m2 = 0, gross_biomass_g_100m2 = 0, weight_g_i = 0, biomass_g_100m2_i = 0, net_biomass_g_100m2 = 0)
   for(i in 1:years){
-    df[i,1] <- spp # spp
-    df[i,2] <- mf # male or female
-    df[i,3] <- i # year post restoration
-    df[i,4] <- if(i<=t_max)(dens*exp(-m*(i-0.5)))else(0) # density of recruited individuals in each year, all die after max age
-    df[i,5] <- if(i<=t_max)(l_asym*(1-exp(-Ks*(i-t_0))))else(0) # estimate length using von bert eqn, only while fish are alive
-    df[i,6] <- if(i<=t_max){a*df[i,5]^b}else(0) # convert length to weight, only if fish is alive
-    df[i,7] <- if(i<=t_max){df[i,4]*df[i,6]}else(0) # convert to biomass 
-    df[i,8] <- if(i==t_harv){df[i,7]}else if(i>t_harv){df[i-1,8]+df[i,7]}else(0) # cumulative biomass gains over time (gross gains)
-    df[i,9] <- if(i>1 & i<=t_max)(df[i,6] - df[i-1,6])else(0) # get the incremental increase in weight for each time interval (i.e. year)
-    df[i,10] <- if(i<=t_max){df[i,4]*df[i,9]}else(0) # convert to incremental biomass gain each year
-    df[i,11] <- if(i==t_harv){df[i,7]}else if(i>t_harv){df[i-1,11]+df[i,10]}else(0) # cumulative incremental gains in biomass (net gains)
-    }
+      df[i+1,1] <- spp # spp
+      df[i+1,2] <- mf # male or female
+      df[i+1,3] <- i # year post restoration
+      df[i+1,4] <- if(i<=t_max)(dens*exp(-m*(i-0.5)))else(0) # density of recruited individuals in each year, all die after max age
+      df[i+1,5] <- if(i<=t_max)(l_asym*(1-exp(-Ks*(i-t_0))))else(0) # estimate length using von bert eqn, only while fish are alive
+      df[i+1,6] <- if(i<=t_max){a*df[i+1,5]^b}else(0) # convert length to weight, only if fish is alive
+      df[i+1,7] <- if(i<=t_max){df[i+1,4]*df[i+1,6]}else(0) # convert to biomass 
+      df[i+1,8] <- if(i==t_harv){df[i+1,7]}else if(i>t_harv){df[i,8]+df[i+1,7]}else(0) # cumulative biomass gains over time (gross gains)
+      df[i+1,9] <- if(i>1 & i<=t_max)(df[i+1,6] - df[i,6])else(0) # get the incremental increase in weight for each time interval (i.e. year)
+      df[i+1,10] <- if(i<=t_max){df[i+1,4]*df[i+1,9]}else(0) # convert to incremental biomass gain each year
+      df[i+1,11] <- if(i==t_harv){df[i+1,7]}else if(i>t_harv){df[i,11]+df[i+1,10]}else(0) # cumulative incremental gains in biomass (net gains)
+  }
   return(df)
 }
 
 # run the model
 
 enhancement <- mod_enhance(dat$dens, dat$species, NA, dat$m, dat$t_max, dat$t_0, 
-                           dat$t_harv, dat$l_asym, dat$Ks, dat$a, dat$b, 
-                           area_restor = area_restor, years = years)
+                           dat$t_harv, dat$l_asym, dat$Ks, dat$a, dat$b, years = years)
 
 
 # have a look 
 
 head(enhancement)
-plot(x = enhancement$year, y = enhancement$gross_biomass_g_ha, col = 'red')
-points(x = enhancement$year, y = enhancement$net_biomass_g_ha)
+plot(x = enhancement$year, y = enhancement$gross_biomass_g_100m2, col = 'red')
+points(x = enhancement$year, y = enhancement$net_biomass_g_100m2)
